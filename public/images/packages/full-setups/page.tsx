10a05476cdf0a2c4789524ee4a5f4658af3dd6da
azurestaycationbysiaos <staycationnearme2023@gmail.com>
2026-08-04 11:59:07 +0800

    Update page.tsx

--- public/images/packages/full-setups/page.tsx
+++ public/images/packages/full-setups/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export const metadata = {
   title: "Full Room Decorations & Themed Set Ups — Portfolio | Azure Staycation by Siaos",
